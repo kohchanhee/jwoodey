9d54991ea0a56ebac9e733b4f829d751438e901f
Jon Woodey <woodeyjon@gmail.com>
2020-08-10 16:36:16 -0500

    Updates

---
+++ precache-manifest.94f2bcd9f67868f0bc5cfc97c3b7c929.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a3d840a497011f4755a0fa1eb52ba3",
+    "url": "/jwoodey/index.html"
+  },
+  {
+    "revision": "2f00ad249c48ad4538ae",
+    "url": "/jwoodey/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5d0565516d6c10001490",
+    "url": "/jwoodey/static/js/2.82417238.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jwoodey/static/js/2.82417238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f00ad249c48ad4538ae",
+    "url": "/jwoodey/static/js/main.55377c28.chunk.js"
+  },
+  {
+    "revision": "529d9878a7a9c1d01da4",
+    "url": "/jwoodey/static/js/runtime-main.8f590aa2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jwoodey/static/media/logo.5d5d9eef.svg"
+  }
+]);
